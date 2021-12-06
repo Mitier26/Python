1eaebe2fbd778ebe3f918fe15ec481635ba3cb36
Mitier26 <mitier26@gmail.com>
2021-12-07 00:00:49 +0900

    2021.12.07 연산자

---
+++ 03_operator3.py
@@ -0,0 +1,10 @@
+print(abs(-5))          # 절대값
+print(pow(3,2))         # 3^2  = 3 * 3
+print(max(5, 14))       # 큰수
+print(min(4,25))        # 작은수
+print(round(3.14))      # 반올림
+
+from math import *      # 수학적 기능을 사용하겠다
+print(floor(4.99))      # 내림
+print(ceil(3.14))       # 올림
+print(sqrt(16))         # 제곱근 4
