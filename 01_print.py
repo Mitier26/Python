a4edd5a9ed56ca780b2b9f74c08c11b7af8f5ca0
Mitier26 <mitier26@gmail.com>
2021-12-06 16:29:25 +0900

    2021.12.05 파이썬 시작

---
+++ 01_print.py
@@ -0,0 +1,16 @@
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+12)
+print(2*8)
+print(3*(3+2))
+
+print('풍선')
+print("토리")
+print("ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ")
+print("ㅋ"*6)
+
+print(5 >10)
+print(5 < 10)
+print(True)
---
+++ 03_operator.py
@@ -0,0 +1,30 @@
+print(1+1)
+print(3-2)
+print(5*3)
+print(9/3)
+
+print(2**3)     # 2^3 = 8, 제곱
+
+print(5%3)      # 나머지
+print(10%3)
+
+print(5//3)     # 몫
+print(10//3)
+
+print(10 > 3)
+print(4 >= 7)
+print(10 < 3)
+print(5 <= 5)
+
+print(4 == 4)   # 같다
+print(1 != 3)   # 다르다
+print(not(1 != 3))
+
+print((3 > 0) and (3 < 5))
+print((3 > 0) & (3 < 5))
+
+print((3 > 0 ) or (3 > 5))
+print((3 > 0 ) | (3 > 5))
+
+print( 5 > 4 > 3)   # 5 > 4 와 4 > 3
+print (5 > 4 > 7)   # 5 > 4 와 4 > 7 <===거짓
